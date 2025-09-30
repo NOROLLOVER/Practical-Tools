7c94e77f1d0fe6699d692c17e69fed6d5c48f27d
NOROLLOVER <1290465417@qq.com>
2025-09-30 16:45:48 +0800

    1

--- utils/study_re.py
+++ utils/study_re.py
@@ -1,4 +1,5 @@
 import re
+from datetime import datetime
 
 r"""
 “.”:匹配单个字符；“*”:匹配前一个字符的0次或多次；
@@ -35,5 +36,38 @@ if __name__ == '__main__':
     all_email = re.findall(email_pattern,text)
     for email in all_email:
         print(email)
+
+    #匹配连在一起的数字
+    numbers = "Age: 25, Height: 180cm, Weight: 75kg"
+    numbers_pattern = r"\d+"
+    print(re.findall(numbers_pattern, numbers))
+
+    # 字符串替换
+    text_with_links = "Visit https://www.baidu.com and http://www.text.org"
+    place_pattern = r"\w+://\w+\.\w+\.\w+"
+    print(re.sub(place_pattern, "[***]", text_with_links))
+
+    # 4. 分组匹配
+    date = datetime.now().date()
+    date_text = f"今天是 {date}"
+    print(date_text)
+    now_date_pattern = r"(\d+)-(\d+)-(\d+)"
+    print(re.findall(now_date_pattern, date_text))
+    for now_date in re.findall(now_date_pattern, date_text):
+        year,month,day = now_date
+        print(f"明天是{year}-{month}-{int(day)+1}")
+
+
+
+    date_pattern = r"(\d{4})-(\d{2})-(\d{2})"
+
+    print(re.findall(date_pattern,date_text))
+
+    # 查找所有日期并提取年、月、日
+    matches = re.findall(date_pattern, date_text)
+    for match in matches:
+        year, month, day = match
+        print(f"日期: {year}年{month}月{day}日")
+
 
 
